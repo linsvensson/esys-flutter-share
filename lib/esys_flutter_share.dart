ac03ce7c7dc5b1247350fbd03ff10fdb4fcba41a
Carolin <carolin.svensson@live.se>
2022-02-23 00:19:44 +0100

    fix constraints for iPad

--- lib/esys_flutter_share.dart
+++ lib/esys_flutter_share.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter/widgets.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -8,20 +9,34 @@ class Share {
   static const MethodChannel _channel = const MethodChannel(
       'channel:github.com/orgs/esysberlin/esys-flutter-share');
 
+  static void maybeAddRectToArgs(
+      Rect? sharePositionOrigin, Map<String, dynamic> args) {
+    if (sharePositionOrigin != null) {
+      args['originX'] = sharePositionOrigin.left.toString();
+      args['originY'] = sharePositionOrigin.top.toString();
+      args['originWidth'] = sharePositionOrigin.width.toString();
+      args['originHeight'] = sharePositionOrigin.height.toString();
+    }
+  }
+
   /// Sends a text to other apps.
-  static void text(String title, String text, String mimeType) {
-    Map argsMap = <String, String>{
+  static void text(String title, String text, String mimeType,
+      {Rect? sharePositionOrigin}) {
+    Map<String, dynamic> argsMap = <String, String>{
       'title': '$title',
       'text': '$text',
       'mimeType': '$mimeType'
     };
+
+    maybeAddRectToArgs(sharePositionOrigin, argsMap);
     _channel.invokeMethod('text', argsMap);
   }
 
   /// Sends a file to other apps.
   static Future<void> file(
-      String title, String name, List<int> bytes, String mimeType, {String text = ''}) async {
-    Map argsMap = <String, String>{
+      String title, String name, List<int> bytes, String mimeType,
+      {String text = '', Rect? sharePositionOrigin}) async {
+    Map<String, dynamic> argsMap = <String, String>{
       'title': '$title',
       'name': '$name',
       'mimeType': '$mimeType',
@@ -32,13 +47,16 @@ class Share {
     final file = await new File('${tempDir.path}/$name').create();
     await file.writeAsBytes(bytes);
 
+    maybeAddRectToArgs(sharePositionOrigin, argsMap);
+
     _channel.invokeMethod('file', argsMap);
   }
 
   /// Sends multiple files to other apps.
   static Future<void> files(
-      String title, Map<String, List<int>> files, String mimeType, {String text = ''}) async {
-    Map argsMap = <String, dynamic>{
+      String title, Map<String, List<int>> files, String mimeType,
+      {String text = '', Rect? sharePositionOrigin}) async {
+    Map<String, dynamic> argsMap = <String, dynamic>{
       'title': '$title',
       'names': files.entries.toList().map((x) => x.key).toList(),
       'mimeType': mimeType,
@@ -52,6 +70,7 @@ class Share {
       await file.writeAsBytes(entry.value);
     }
 
+    maybeAddRectToArgs(sharePositionOrigin, argsMap);
     _channel.invokeMethod('files', argsMap);
   }
 }
